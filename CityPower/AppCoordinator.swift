359fcc3008a36b6aa2764b4f0627436b7220c45e
naiboomkrub <78785697+naiboomkrub@users.noreply.github.com>
2021-02-19 11:39:33 +0700

    Delete AppCoordinator.swift

--- CityPower/AppCoordinator.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppCoordinator.swift
-//  CityPower
-//
-//  Created by Natcha Watcharawittayakul on 27/11/2563 BE.
-//  Copyright © 2563 City Power. All rights reserved.
-//
-
-import Foundation
